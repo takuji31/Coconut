2224dbf0c2810683a0faceb0eae7447b844e10ab
Nishibayashi Takuji <takuji@senchan.jp>
2012-04-15 10:17:15 +0900

    とりあえず作業内容詰め込んでおく

--- app.pl
+++ app.pl
@@ -1,6 +1,11 @@
 #!/usr/bin/env perl
 use Mojolicious::Lite;
 
+use Text::Markdown;
+use File::Slurp qw/read_file/;
+use Data::Dumper;
+use YAML::Syck;
+
 app->secret('Coconut hogehogefugafuga');
 
 plugin 'xslate_renderer' => {
@@ -13,25 +18,29 @@ plugin 'xslate_renderer' => {
 };
 
 get '/' => sub {
-  my $self = shift;
+    my $self = shift;
 } => 'index';
 
+get '/slide/' => sub {
+    my $self = shift;
+    my $path = $self->app->home->rel_file('slide.mkd');
+
+    my $text = read_file($path);
+    my @templates = split /\n-----\n/m, $text;
+    my $meta = YAML::Syck::Load(shift @templates);
+    my @slides;
+    for my $template (@templates) {
+        my $mkd = Text::Markdown->new->markdown($template);
+        push @slides, $mkd;
+    }
+    $self->stash(meta => $meta);
+    $self->stash(slides => \@slides);
+} => 'slide';
+
+get '/tool/:page' => sub {
+    my $self = shift;
+    my $path = $self->app->home->rel_file('slide.md');
+    say $path;
+} => 'slide';
+
 app->start;
-__DATA__
-
-@@ index.html.tx
-:cascade layout
-:around content -> {
-Welcome to the Mojolicious real-time web framework!
-:}
-
-@@ layout.tx
-<!DOCTYPE html>
-<html>
-  <head><title><: block title -> {} :></title></head>
-  <body>
-    :block content -> {
-
-    :}
-  </body>
-</html>
